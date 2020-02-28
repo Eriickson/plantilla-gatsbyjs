19f567846135b1fcbcfea3e21ccccc1258940261
Erickson Manuel <Erickson01d@gmail.com>
2020-02-28 06:59:59 -0500

    Configurando Tailwindcss para styled components

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,7 +27,12 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    `gatsby-plugin-postcss`,
+    {
+      resolve: `gatsby-plugin-postcss`,
+      options: {
+        // Accepts all options defined by `gatsby-plugin-postcss` plugin.
+      },
+    },
     {
       resolve: `gatsby-plugin-react-redux`,
       options: {
@@ -41,7 +46,12 @@ module.exports = {
         windowKey: "__PRELOADED_STATE__",
       },
     },
-    `gatsby-plugin-emotion`,
+    {
+      resolve: `gatsby-plugin-emotion`,
+      options: {
+        // Accepts all options defined by `babel-plugin-emotion` plugin.
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
--- src/components/CountRedux.jsx
+++ src/components/CountRedux.jsx
@@ -2,6 +2,16 @@ import React from "react"
 import * as actions from "../actions/contadorActions"
 import { useSelector, useDispatch } from "react-redux"
 import { css } from "@emotion/core"
+import tw from "tailwind.macro"
+
+const Button = tw.button`
+  bg-gray-800
+  text-white
+  text-sm
+  px-4
+  py-2
+  border
+`
 
 const CountRedux = () => {
   const dispatch = useDispatch()
@@ -10,26 +20,21 @@ const CountRedux = () => {
   return (
     <div className="w-screen h-screen flex justify-center items-center">
       <div>
-        <button
-          className="bg-gray-800 text-white text-sm px-4 py-2 border rounded-bl rounded-tl"
+        <Button
+          className="rounded-bl rounded-tl"
           onClick={() => dispatch(actions.disminuir(cuenta))}
         >
           -
-        </button>
-        <span
-          className="px-8 border-t border-b py-2"
-          css={css`
-            color: red;
-          `}
-        >
+        </Button>
+        <span className="px-8 border-t border-b py-2" css={tw`bg-red-500`}>
           {cuenta}
         </span>
-        <button
-          className="bg-gray-800 text-white text-sm px-4 py-2 border rounded-br rounded-tr"
+        <Button
+          className="rounded-br rounded-tr"
           onClick={() => dispatch(actions.aumentar(cuenta))}
         >
           +
-        </button>
+        </Button>
       </div>
     </div>
   )
