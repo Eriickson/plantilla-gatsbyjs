3216b40bf99229c8aa07f509322aa5010938a61a
Erickson Manuel <Erickson01d@gmail.com>
2020-02-28 15:18:05 -0500

    Agregando custom form a tailwindcss

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,6 +4,16 @@ import CountRedux from "../components/CountRedux"
 const index = () => {
   return (
     <div>
+      <input
+        className="m-12 form-checkbox outline-none"
+        type="checkbox"
+        name=""
+        id=""
+      />
+      <select class="form-select block w-1/3 m-auto mt-1">
+        <option>Option 1</option>
+        <option>Option 2</option>
+      </select>
       <CountRedux />
     </div>
   )
