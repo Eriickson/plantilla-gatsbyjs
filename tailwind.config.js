bbb885774044f8e7ef22be7a5caac46cc2c6f6e9
Erickson - Casa <erickson01d@gmail.com>
2020-03-20 18:44:55 -0400

    agregando sombras y configurando colores para tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,31 @@
 module.exports = {
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          "100": "",
+          "200": "",
+          "300": "",
+          "400": "",
+          "500": "",
+          "600": "",
+          "700": "",
+          "800": "",
+          "900": "",
+        },
+        secondary: {
+          "100": "",
+          "200": "",
+          "300": "",
+          "400": "",
+          "500": "",
+          "600": "",
+          "700": "",
+          "800": "",
+          "900": "",
+        },
+      },
+    },
     screens: {
       xxs: "320px",
       // => @media (min-width: 320px) { ... }
@@ -22,6 +47,20 @@ module.exports = {
 
       xxl: "1440px",
       // => @media (min-width: 1440px) { ... }
+    },
+    boxShadow: {
+      "outline-black": "0 0 0 3px rgba(0, 0, 0, 0.5)",
+      "outline-white": "0 0 0 3px rgba(255, 255, 255, 0.5)",
+      "outline-gray": "0 0 0 3px rgba(160, 174, 192, 0.5)",
+      "outline-red": "0 0 0 3px rgba(245, 101, 101, 0.5)",
+      "outline-orange": "0 0 0 3px rgba(237, 137, 54, 0.5)",
+      "outline-yellow": "0 0 0 3px rgba(236, 201, 75, 0.5)",
+      "outline-green": "0 0 0 3px rgba(72, 187, 120, 0.5)",
+      "outline-teal": "0 0 0 3px rgba(56, 178, 172, 0.5)",
+      "outline-blue": "0 0 0 3px rgba(66, 153, 225, 0.5)",
+      "outline-indigo": "0 0 0 3px rgba(102, 126, 234, 0.5)",
+      "outline-purple": "0 0 0 3px rgba(159, 122, 234, 0.5)",
+      "outline-pink": "0 0 0 3px rgba(237, 100, 166, 0.5)",
     },
   },
   variants: {},
