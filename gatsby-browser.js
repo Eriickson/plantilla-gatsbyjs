4529949996229cef57565287d7e8051baf4d3f93
Erickson - Casa <erickson01d@gmail.com>
2020-03-20 18:56:12 -0400

    Agregando Bootstrap

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -6,4 +6,7 @@
 
 // You can delete this file if you're not using it
 import "./src/styles/globals.css"
+import "jquery/dist/jquery.min.js"
+import "popper.js/dist/popper.min"
+import "bootstrap/dist/js/bootstrap.min.js"
 export { wrapRootElement } from "./src/apollo/WrapRootElement"
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,28 +1,7 @@
-import React, { useState } from "react"
-import CountRedux from "../components/CountRedux"
-import withLocation from "../tools/withLocation"
-import Header from "../components/Header"
+import React from "react"
 
-const Index = ({ search }) => {
-  const [parmsUrl] = useState(search)
-  console.log(parmsUrl)
-
-  return (
-    <div>
-      <Header title={"Home"} />
-      <input
-        className="m-12 form-checkbox outline-none"
-        type="checkbox"
-        name=""
-        id=""
-      />
-      <select className="form-select block w-1/3 m-auto mt-1">
-        <option>Option 1</option>
-        <option>Option 2</option>
-      </select>
-      <CountRedux />
-    </div>
-  )
+const Index = () => {
+  return <div>hola a todos</div>
 }
 
-export default withLocation(Index)
+export default Index
